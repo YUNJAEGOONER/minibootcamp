4ec5a1e5074b37d61d748c601878d882cd14227a
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-08-08 13:58:38 +0900

    enhancement : #4 핫 게시판 구현

--- src/main/java/org/example/project1/home/HomeController.java
+++ src/main/java/org/example/project1/home/HomeController.java
@@ -1,35 +1,34 @@
 package org.example.project1.home;
 
-import java.security.Principal;
-
-import org.example.project1.answer.Answer;
-import org.example.project1.answer.AnswerForm;
 import org.example.project1.question.Question;
-import org.example.project1.user.SiteUser;
+import org.example.project1.question.QuestionService;
+import org.example.project1.user.UserService;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/home")
+@RequiredArgsConstructor
 public class HomeController {
+	private final UserService userService;
+	private final QuestionService questionService;
 
 	@PreAuthorize("isAuthenticated()")
 	@GetMapping("/main")
-	public String main_home() {
+	public String main_home(Model model) {
 		if (SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken) {
 			return "redirect:/";
 		}
+		Page<Question> hotList = questionService.getTop5HotList();
+		model.addAttribute("questions", hotList);
 		return "home";
 	}
 }
--- src/test/java/org/example/project1/Project1ApplicationTests.java
+++ src/test/java/org/example/project1/Project1ApplicationTests.java
@@ -1,13 +1,31 @@
 package org.example.project1;
 
+import org.example.project1.question.QuestionRepository;
+import org.example.project1.question.QuestionService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Project1ApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Autowired
+	private QuestionService questionService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testJpa() {
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content, null);
+		}
 	}
 
 }
